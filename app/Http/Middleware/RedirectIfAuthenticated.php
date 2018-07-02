74259e118cd484f5791e71107a2b70a1d79aac84
Victor <victorwhx@126.com>
2018-07-02 12:24:05 +0000

    Finish user edit, updata,index and destroy actions

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,8 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            session()->flash('info', '您已成功登录，无需重复操作。');
+            return redirect('/');
         }
 
         return $next($request);
